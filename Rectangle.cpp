7eaa177ecf048dfef44cf5215bf33be150a0aac4
ryanphadnis <phadrc18@wfu.edu>
2019-11-07 15:07:23 -0500

    Followed instructions given in class

---
+++ Rectangle.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Ryan Phadnis on 11/7/19.
+//
+#include "Rectangle.h"
+
+int Rectangle :: area()
+{
+    return width * height;
+}
---
+++ Triangle.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Ryan Phadnis on 11/7/19.
+//
+#include "Triangle.h"
+
+int Triangle :: area()
+{
+    return (width * height) / 2;
+}
